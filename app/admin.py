d1d165d1b1f0a377093aaee308f0533d0c0fb772
Михаил Мяделец <mrr.myadelets@mail.ru>
2022-11-25 00:38:12 +0300

    add Like in admin

--- app/admin.py
+++ app/admin.py
@@ -6,5 +6,6 @@ admin.site.register(models.Profile)
 admin.site.register(models.Tag)
 admin.site.register(models.Question)
 admin.site.register(models.Answer)
+admin.site.register(models.Like)
 
 # Register your models here.
